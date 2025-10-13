53a40b17ec50ffbe3801842e8c7c8bc0c870d163
Masudur400 <md8004210@gmail.com>
2025-10-13 21:15:59 +0600

    git clone and file setup

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,4 +7,14 @@ export default {
     node_env: process.env.NODE_ENV,
     port: process.env.PORT,
     database_url: process.env.DATABASE_URL,
+    JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
+    JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
+    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
+    JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES as string,
+    cloudinary: {
+        api_secret: process.env.CLOUDINARY_API_SECRET,
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY
+    },
+    BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string
 }
